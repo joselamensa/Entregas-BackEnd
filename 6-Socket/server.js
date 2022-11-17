ec41d21b14bfce56f95a9999b59a631b8aadfdd4
joselamensa <joselamensa10@gmail.com>
2022-11-17 18:32:44 -0300

    server e index

--- 6-Socket/server.js
+++ 6-Socket/server.js
@@ -8,17 +8,16 @@ const ContenedorMsg = require('./container/contenedorMsg');
 const moment = require('moment');
 const timestamp = moment().format('h:mm a');
 
-const datas = new Contenedor;
-const dataMsg = new ContenedorMsg;
+const datas = new Contenedor();
+const dataMsg = new ContenedorMsg();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Implementacion del server con socket
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
 
-httpServer.listen(PORT, () => console.log("SERVER ON http://localhost:" + PORT));
+httpServer.listen(PORT, () => console.log('SERVER ON http://localhost:' + PORT));
 
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -38,45 +37,26 @@ app.get('/', async (req, res) => {
   res.render('productslist');
 });
 
-
-
-// Corre cuando se conecta un cleinte
-io.on("connection", async (socket) => {
+io.on('connection', async (socket) => {
   console.log(`Nuevo cliente conectado ${socket.id}`);
 
-  // Muestra la lista completa de productos al cliente
-  socket.emit("product-list", await datas.getAll());
+  socket.emit('product-list', await datas.getAll());
 
-  // Muestra el historial completo de mensajes al cliente
-  socket.emit("msg-list", await dataMsg.getAll());
+  socket.emit('msg-list', await dataMsg.getAll());
 
-  // Recibe prodcuto del cliente
-  socket.on("product", async (data) => {
-
-    // Guarda el producto nuevo en productos.json
+  socket.on('product', async (data) => {
     await datas.save(data);
 
-    // Muestra el mensaje por consola
-    console.log('Se recibio un producto nuevo', "producto:", data);
+    console.log('Se recibio un producto nuevo', 'producto:', data);
 
-    // Devuelve el historial completo de mensajes al cliente con el nuevo mensaje
-    io.emit("product-list", await datas.getAll());
-
+    io.emit('product-list', await datas.getAll());
   });
 
-  // Recibe mensaje del cliente
-  socket.on("msg", async (data) => {
-
-    // Guarda en mensaje nuevo en mensajes.json
+  socket.on('msg', async (data) => {
     await dataMsg.save({ socketid: socket.id, timestamp: timestamp, ...data });
 
-    // Muestra el mensaje por consola
-    console.log('Se recibio un msg nuevo', "msg:", data);
+    console.log('Se recibio un msg nuevo', 'msg:', data);
 
-    // Devuelve el historial completo de mensajes al cliente con el nuevo mensaje
-    io.emit("msg-list", await dataMsg.getAll());
-
+    io.emit('msg-list', await dataMsg.getAll());
   });
 });
-
-
