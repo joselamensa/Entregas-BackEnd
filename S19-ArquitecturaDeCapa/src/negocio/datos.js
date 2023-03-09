57fed4bf85b438a1177f55a5b904c633243ee9f6
joselamensa <joselamensa10@gmail.com>
2023-03-09 17:09:37 -0300

    complemento entrega N°20

--- S19-ArquitecturaDeCapa/src/negocio/datos.js
+++ S19-ArquitecturaDeCapa/src/negocio/datos.js
@@ -1,44 +1,50 @@
-import { recuperarTodos, guardar, guardarUser, getUser, getDatos } from '../persistencia/datos.js';
-import { v4 as uuidv4 } from 'uuid';
-
+import {
+  recuperarTodos,
+  guardar,
+  guardarUser,
+  getUser,
+  getDatos,
+} from "../persistencia/datos.js";
+import { v4 as uuidv4 } from "uuid";
 
 async function obtenerDatos() {
-    return await recuperarTodos()
+  return await recuperarTodos();
 }
 
 async function crearDato(dato) {
-    dato.added = Date.now()
-    await guardar(dato)
-    return dato
+  dato.added = Date.now();
+  await guardar(dato);
+  return dato;
 }
-
 
 const guardarDatos = async (datos) => {
-    return await guardar(datos)
-}
+  return await guardar(datos);
+};
 
 const guardarUsuario = async (email, pass) => {
-    const clave = uuidv4()
-    await guardarUser(email, pass, clave)
-    return { clave }
-}
-
+  const clave = uuidv4();
+  await guardarUser(email, pass, clave);
+  return { clave };
+};
 
 const listarOperaciones = async (clave) => {
-    if (!clave) return { error: 'Debes ingresar tu clave' };
+  if (!clave) return { error: "Debes ingresar tu clave" };
 
-    const user = await getUser(clave);
+  const user = await getUser(clave);
 
-    console.log({ user })
+  console.log({ user });
 
-    if (!user || !user.length) return { error: 'No existe tu usuario' }
+  if (!user || !user.length) return { error: "No existe usuario" };
 
+  const datos = await getDatos();
 
-    const datos = await getDatos();
+  return { datos };
+};
 
-    return { datos }
-}
-
-
-
-export { obtenerDatos, crearDato, guardarDatos, guardarUsuario, listarOperaciones }
+export {
+  obtenerDatos,
+  crearDato,
+  guardarDatos,
+  guardarUsuario,
+  listarOperaciones,
+};
